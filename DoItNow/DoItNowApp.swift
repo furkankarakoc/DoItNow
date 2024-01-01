1fb7d9ba6b408bf82e3efdd1ab2620397eac8943
Furkan Karakoc <44609894+furkankarakoc@users.noreply.github.com>
2024-01-01 21:30:40 +0300

    Initial Commit

---
+++ DoItNow/DoItNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItNowApp.swift
+//  DoItNow
+//
+//  Created by KFA8IB on 1.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoItNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
